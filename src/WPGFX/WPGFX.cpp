801bdd24e30ba4361ede0c2d3fc9dec00a935271
Lukas Fülling <lukas@k40s.net>
2021-02-05 23:04:54 +0100

    properly fix error graphic

--- src/WPGFX/WPGFX.cpp
+++ src/WPGFX/WPGFX.cpp
@@ -45,9 +45,9 @@ void WPGFX::showFatalError(String error, bool reboot)
     h = tft->height();
 
     tft->setCursor(W / 2 - 64, h / 2 + 9);
-    tft->println("-        -");
+    tft->println("-         -");
     tft->setCursor(W / 2 - 64, h / 2 + 18);
-    tft->println(" \\_x.x)_/ ");
+    tft->println(" \\(_x.x)_/ ");
 
     tft->setFont(&gidugu10pt7b);
     tft->setCursor(W / 2 - 56, h / 2 + 40);
